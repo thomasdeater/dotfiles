46f45a3a0a9277d8d6fc91d9513d4d7c679632f0
thomasdeater <thomasdeater@gmail.com>
2019-06-20 19:53:08 -0400

    some updates, adding dart to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/.pub-cache/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/Code/solr-7.4.0/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
